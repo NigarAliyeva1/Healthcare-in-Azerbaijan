b6156db69fedd099b2d2146793e3091cc0d3e797
Nigarkhanim Aliyeva <112957859+NigarAliyeva1@users.noreply.github.com>
2024-10-22 22:57:36 +0400

    Create avg_age_of_patients_by_gender.sql

---
+++ SQL/avg_age_of_patients_by_gender.sql
@@ -0,0 +1,6 @@
+
+--Avg age of patients who didn't call hospital by gender
+select gender,called_hospital,
+round(avg(age)) as avg_age from patients p
+where called_hospital ='No'
+group by gender,called_hospital;
