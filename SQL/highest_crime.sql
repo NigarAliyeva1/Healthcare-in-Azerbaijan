497305ba3d2d45e4244b17bc137edcd9ab0dc676
Nigarkhanim Aliyeva <112957859+NigarAliyeva1@users.noreply.github.com>
2024-10-22 23:01:22 +0400

    Create highest_crime.sql

---
+++ SQL/highest_crime.sql
@@ -0,0 +1,7 @@
+--TOP 5 highest crime percentage 2023
+with top_5 as (
+SELECT c.region_id,c.region,round(c.year_2023/p.people_count *100,5) as percentage_of_crime
+FROM crime c
+left join population p on c.region_id=p.region_id
+ORDER BY percentage_of_crime DESC
+FETCH FIRST 5 ROWS ONLY)
